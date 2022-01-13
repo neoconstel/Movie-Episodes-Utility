dea69f65abdc790c27fd74fcee2fd0884556acd6
neoconstel <neoconstel@gmail.com>
2022-01-13 22:09:13 +0100

    added confirm button

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 import tkinter
 
-from click import command
+unselected_color = "grey"
+selected_color = "green"
 
 class Ui:
     def __init__(self, episode:str):
@@ -30,18 +31,33 @@ class Ui:
                 self.number_buttons.append(number_button)
 
 
-                number_button.config(text=character, font=("Arial", 9, "bold"), bg="gray")
+                number_button.config(text=character, font=("Arial", 9, "bold"), bg=unselected_color)
                 number_button.grid(row=row, column=column)
             else:
                 character_label = tkinter.Label()
                 character_label.config(text=character, font=("Arial", 9, "bold"))
                 character_label.grid(row=row, column=column)
             column += 1
+        row += 1
+        column = 0
 
+        gap_label = tkinter.Label()
+        gap_label.config(text=" ", font=("Arial", 9, "bold"))
+        gap_label.grid(row=row, column=column, columnspan=len(episode))
+        row += 1
+
+        confirm_button = tkinter.Button()
+        confirm_button.config(text="Confirm", font=("Arial", 9, "bold"), bg=unselected_color)
+        confirm_button.grid(row=row, column=column, columnspan=len(episode))        
+
+        # button functions -- decorated to target each button separately
         for i, btn in enumerate(self.number_buttons):
             def action(index=i):
                 def color_toggle():
-                    self.number_buttons[index].config(bg="green")
+                    if self.number_buttons[index]["bg"] == unselected_color:
+                        self.number_buttons[index].config(bg=selected_color)
+                    else:
+                        self.number_buttons[index].config(bg=unselected_color)
                 return color_toggle
 
             self.number_buttons[i].config(command=action(i))
