a26c87d25b0c941600865553837eeb50efe1857e
neoconstel <neoconstel@gmail.com>
2022-01-16 18:44:54 +0100

    Added setup.sh and uninstall.sh for installing/uninstalling on unix systems

---
+++ setup.sh
@@ -0,0 +1,23 @@
+sudo echo
+echo installing movie episode utility
+
+export APP=episode-utility
+sudo mkdir $APP
+sudo cp *.py $APP
+sudo python3 -m zipapp $APP
+sudo chmod 777 $APP*
+sudo rm -r $APP
+
+export APP_DIR="/usr/share"
+sudo mkdir $APP_DIR/$APP
+sudo mv $APP.pyz $APP_DIR/$APP
+
+export APP_ENTRY=episode-utility
+sudo touch $APP_ENTRY
+sudo chmod 777 $APP_ENTRY
+sudo echo "#!/bin/bash" > $APP_ENTRY
+sudo echo "python3 $APP_DIR/$APP/$APP.pyz" >> $APP_ENTRY
+export APP_ENTRY_DIR="/usr/bin"
+sudo mv $APP_ENTRY $APP_ENTRY_DIR
+echo movie episode utility successfully installed. Run $APP_ENTRY from terminal to launch.
+
---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+sudo echo
+echo installing movie episode utility
+
+export APP=episode-utility
+export APP_DIR="/usr/share"
+sudo rm -r "$APP_DIR/$APP"
+
+export APP_ENTRY=episode-utility
+export APP_ENTRY_DIR="/usr/bin"
+sudo rm "$APP_ENTRY_DIR/$APP_ENTRY"
+echo movie episode utility successfully uninstalled
+
