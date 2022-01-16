b83c549a1aef554ac9c06085fe426d6ad719ba26
neoconstel <neoconstel@gmail.com>
2022-01-16 20:55:22 +0100

    setup now embeds uninstall routine in the installation

--- setup.sh
+++ setup.sh
@@ -12,11 +12,17 @@ export APP_DIR="/usr/share"
 sudo mkdir $APP_DIR/$APP
 sudo mv $APP.pyz $APP_DIR/$APP
 
+sudo cp uninstall.sh $APP_DIR/$APP
+
 export APP_ENTRY=episode-utility
 sudo touch $APP_ENTRY
 sudo chmod 777 $APP_ENTRY
-sudo echo "#!/bin/bash" > $APP_ENTRY
-sudo echo "python3 $APP_DIR/$APP/$APP.pyz" >> $APP_ENTRY
+sudo echo "#!/bin/bash
+if [ \"\$1\" == \"--uninstall\" ]; then
+	sudo $APP_DIR/$APP/uninstall.sh
+else
+	python3 $APP_DIR/$APP/$APP.pyz
+fi" > $APP_ENTRY
 export APP_ENTRY_DIR="/usr/bin"
 sudo mv $APP_ENTRY $APP_ENTRY_DIR
 echo movie episode utility successfully installed. Run \"$APP_ENTRY\" from terminal to launch.
